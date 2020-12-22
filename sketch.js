d3d045270f6ef8937942116aacc9c250e1cb88b3
waiscodes <b.wais@outlook.com>
2020-12-22 15:42:41 -0700

    Added player turns

--- sketch.js
+++ sketch.js
@@ -1,15 +1,30 @@
 let board = [
   ["", "", ""],
-  ["", "O", "X"],
-  ["", "X", ""],
+  ["", "", ""],
+  ["", "", ""],
 ];
 
 let players = ["X", "O"];
-
 let currentPlayer;
+let available = [];
+
 function setup() {
   createCanvas(400, 400);
   currentPlayer = random(players);
+  for (let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++) {
+      available.push([i, j]);
+    }
+  }
+}
+
+function nextTurn() {
+  let index = floor(random(available.length));
+  let spot = available.splice(index, 1)[0];
+  let i = spot[0];
+  let j = spot[1];
+  board[i][j] = players[currentPlayer];
+  currentPlayer = (currentPlayer + 1) % players.length;
 }
 
 function draw() {
@@ -39,4 +54,6 @@ function draw() {
       }
     }
   }
+
+  nextTurn();
 }
