3f682424eeba7e4fbf7a6987ddfa618489bd0185
waiscodes <b.wais@outlook.com>
2020-12-22 15:53:43 -0700

    added a check winner method

--- sketch.js
+++ sketch.js
@@ -18,6 +18,38 @@ function setup() {
   }
 }
 
+function checkWinner() {
+  let winner = null;
+
+  // rows
+  for (let i = 0; i < 3; i++) {
+    if (equals3(board[i][0], board[i][1], board[i][2])) {
+      winner = board[i][0];
+    }
+  }
+
+  // columns
+  for (let i = 0; i < 3; i++) {
+    if (equals3(board[0][i], board[1][i], board[2][i])) {
+      winner = board[0][i];
+    }
+  }
+
+  // diagonals
+  if (equals3(board[0][0], board[1][1], board[2][2])) {
+    winner = board[0][0];
+  }
+  if (equals3(board[2][0], board[1][1], board[0][2])) {
+    winner = board[2][0];
+  }
+
+  if (winner == null && available.length == 0) {
+    return "tie";
+  } else {
+    return winner;
+  }
+}
+
 function nextTurn() {
   let index = floor(random(available.length));
   let spot = available.splice(index, 1)[0];
