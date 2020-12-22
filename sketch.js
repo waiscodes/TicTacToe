65f5acd5f095d1245ef90f0880285bea7d6897c2
waiscodes <b.wais@outlook.com>
2020-12-22 15:14:05 -0700

    rendered the x and os

--- sketch.js
+++ sketch.js
@@ -1,12 +1,40 @@
-let board = [["X", "X", "O"], [("X", "O", "O")], ["O", "O", "X"]];
+let board = [
+  ["", "", ""],
+  ["", "O", "X"],
+  ["", "X", ""],
+];
 
-let player1 = "X";
-let player2 = "O";
+let players = ["X", "O"];
 
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  let w = width / 3;
+  let h = height / 3;
+  strokeWeight(4);
+
+  line(w, 0, w, height);
+  line(w * 2, 0, w * 2, height);
+  line(0, h, width, h);
+  line(0, h * 2, width, h * 2);
+
+  for (let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++) {
+      let x = w * i + w / 2;
+      let y = h * j + h / 2;
+      let spot = board[i][j];
+      textSize(32);
+      let r = w / 4;
+      if (spot == players[1]) {
+        noFill();
+        ellipse(x, y, r * 2);
+      } else if (spot == players[0]) {
+        line(x - r, y - r, x + r, y + r);
+        line(x + r, y - r, x - r, y + r);
+      }
+    }
+  }
 }
